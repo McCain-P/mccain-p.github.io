f9e97c10a56fef26e89a95a5f80a5fe27bbf1fb6
stevestevenson <stevestevenson13570@gmail.com>
2019-09-26 01:53:57 -0700

    Update info.js

--- stem2019CarWebsite/info.js
+++ stem2019CarWebsite/info.js
@@ -1,7 +1,6 @@
 var n = 0;
 window.addEventListener("keydown", function(e) {
 	var key = e.key;
-	console.log(key);
 	var characterIndexes = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 	if (characterIndexes[n] === key) {
 		n++;
