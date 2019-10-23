c8e52f9f8af1e42eb89c09b22d5c0ee27fe2d2d9
stevestevenson <stevestevenson13570@gmail.com>
2019-10-23 15:32:10 -0700

    Update info2.js

--- stem2019CarWebsite/info2.js
+++ stem2019CarWebsite/info2.js
@@ -1,1 +1,13 @@
-
+var n = 0;
+window.addEventListener("keydown", function(e) {
+	var key = e.key;
+	var characterIndexes = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+	if (characterIndexes[n] === key) {
+		n++;
+	} else {
+		n = 0;
+	};
+	if (n === characterIndexes.length) {
+		
+	};
+});
