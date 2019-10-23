e7a61ce2fed76d8b731011c805d8ea7867745303
stevestevenson <stevestevenson13570@gmail.com>
2019-10-23 15:39:01 -0700

    Update info2.js

--- stem2019CarWebsite/info2.js
+++ stem2019CarWebsite/info2.js
@@ -8,6 +8,29 @@ window.addEventListener("keydown", function(e) {
 		n = 0;
 	};
 	if (n === characterIndexes.length) {
-		
+		var image = document.createElement("img");
+		image.src = "resources/testImage.png";
+		image.style.height = "10vh";
+		image.style.width = "auto";
+		image.style.position = "fixed";
+		image.style.top = "50vh";
+		image.style.left = "50vw";
+		var imagePosition = [50, 50];
+		window.addEventListener("keydown", function(e) {
+			var directionKey = e.key;
+			if (directionKey === "ArrowUp") {
+				imagePosition[1]--;
+				image.style.top = String(imagePosition[1]) + "vh";
+			} else if (directionKey === "ArrowDown") {
+				imagePosition[1]++;
+				image.style.top = String(imagePosition[1]) + "vh";
+			} else if (directionKey === "ArrowLeft") {
+				imagePosition[0]--;
+				image.style.left = String(imagePosition[0]) + "vh";
+			} else if (directionKey === "ArrowRight") {
+				imagePosition[0]++;
+				image.style.left = String(imagePosition[0]) + "vh";
+			};
+		});
 	};
 });
